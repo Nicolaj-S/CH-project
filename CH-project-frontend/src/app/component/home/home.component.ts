3fc5ee9e3b55935bb5a5744bc3f213b9e09086e2
Nicolaj Schønberg <48961648+Tristan1449@users.noreply.github.com>
2022-05-08 01:30:23 +0200

    carousel

--- CH-project-frontend/src/app/component/home/home.component.ts
+++ CH-project-frontend/src/app/component/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   title = 'Coffee House';
-  constructor() { }
+  images = [1,2,3].map((n) => `../../../assets/Images/imageSlider${n}.jpg`)
 
-  ngOnInit(): void {
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+    }
   }
 
 }
